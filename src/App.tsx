1a0feb7524038c3c1347aff21a7b6ce0296ea75e
nikhilimts <nikhil@imtsinstitute.com>
2025-09-12 18:33:59 +0530

    basic

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import AdminDashboard from './pages/AdminDashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+
+
+function App() {
+
+
+  return (
+    <>
+       {/* <Login/>
+       <Signup/> */}
+       <AdminDashboard/>
+    </>
+  )
+}
+
+export default App
