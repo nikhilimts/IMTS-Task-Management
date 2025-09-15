f9b6c3de56620ba84b757a7c72e719949912991f
Siddhantji <teotiasiddhant@gmail.com>
2025-09-15 13:29:39 +0530

    Refactor App component to comment out unused imports and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import './App.css'
-import AdminDashboard from './pages/AdminDashboard'
+// import AdminDashboard from './pages/AdminDashboard'
 import Login from './pages/Login'
-import Signup from './pages/Signup'
+// import Signup from './pages/Signup'
 
 
 
 function App() {
 
-
   return (
     <>
-       {/* <Login/>
-       <Signup/> */}
-       <AdminDashboard/>
+       <Login/>
+       {/* <Signup/> */}
+       {/* <AdminDashboard/> */}
     </>
   )
 }
